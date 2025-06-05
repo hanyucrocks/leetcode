ba5831b85247f5622d0f2235e2921e994c05c63f
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-05 16:46:28 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/version/cpp/42-TrappingRainWater_20250605_164626.cpp
@@ -0,0 +1,21 @@
+// Last updated: 6/5/2025, 4:46:26 PM
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int lmax = 0, rmax = 0, total = 0, l = 0, r = n -1;
+        while(l < r){
+            if(height[l] < height[r]){
+                if(lmax > height[l]) total += lmax - height[l];
+                else lmax = height[l];
+                l++;
+            }
+            else{
+                if(rmax > height[r]) total += rmax - height[r];
+                else rmax = height[r];
+                r--;
+            }
+        }
+        return total;
+    }
+};
