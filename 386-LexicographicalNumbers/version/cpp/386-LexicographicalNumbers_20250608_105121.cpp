663151dd69bf3889c889c25f04a3c1dd24fa49d4
hanyuc // xiorra <146408033+hanyucrocks@users.noreply.github.com>
2025-06-08 10:51:22 +0530

    Create 386-LexicographicalNumbers

---
+++ 386-LexicographicalNumbers/version/cpp/386-LexicographicalNumbers_20250608_105121.cpp
@@ -0,0 +1,12 @@
+// Last updated: 6/8/2025, 10:51:21 AM
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> nums;
+        for(int i = 1; i <= n; i++) nums.push_back(i);
+        sort(nums.begin(), nums.end(), [](int a, int b){
+            return to_string (a) < to_string(b);
+        });
+        return nums;
+    }
+};
